34ed6136398055bc653312115a94c998988395b5
i741978 <johannes.kling@sap.com>
2024-11-28 12:25:39 +0100

    added c8 for code coverage

--- my-app/vite.config.ts
+++ my-app/vite.config.ts
@@ -7,6 +7,10 @@ export default defineConfig({
 		globals: true,        // Aktiviert globale Funktionen wie expect
 		environment: 'jsdom', // Stellt DOM-Funktionen zur Verfügung
 		setupFiles: './test/setup.ts', // Initialisierung
+		coverage: {
+			reporter: ['text', 'json', 'html'], // Generate text summary, JSON, and HTML reports
+			reportsDirectory: './coverage',   // Save reports in the `coverage` directory
+		},
 	  },
 	resolve: process.env.VITEST
 	? {
